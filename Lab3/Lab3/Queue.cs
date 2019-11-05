caadf5c6caf83fd06109c0a41cb54ea535e03386
Sebastian Van Der Est <svanderest1@ncstudents.niagaracollege.ca>
2019-11-05 12:00:14 -0500

    added queue and item

---
+++ Lab3/Lab3/Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Queue
+    {
+        public Queue(ToDo first)
+        {
+            First = first;
+        }
+
+        public int Count
+        {
+            get
+            {
+                if (First == null)
+                    return 0;
+                else
+                {
+                    int c = 0;
+                    ToDo i = First;
+                    while (i.Next != null)
+                        c++;
+                    return c;
+                }
+            }
+        }
+        public ToDo First { get; private set; }
+        public void Enqueue(ToDo item)
+        {
+            if (First == null)
+                First = item;
+            else
+            {
+                ToDo i = First;
+                while (i.Next != null)
+                    i = i.Next;
+                i.Next = item;
+            }
+        }
+        public ToDo Dequeue()
+        {
+            ToDo item = First;
+            First = item.Next;
+            return item;
+        }
+
+        public ToDo Peek()
+        {
+            return First;
+        }
+
+    }
+}
---
+++ Lab3/Lab3/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class ToDo
+    {
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public ToDo Next { get; set; }
+    }
+}
